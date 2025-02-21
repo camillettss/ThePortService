8de8c086d4cd4cb5b703ac82378a0a8b9ce2a616
camillettss <70108043+camillettss@users.noreply.github.com>
2025-02-21 23:10:42 +0100

    Aggiunto riconoscimento prenotazioni duplicate

--- app/api/booking/route.js
+++ app/api/booking/route.js
@@ -7,6 +7,20 @@ export async function POST(req) {
         const body = await req.json();
         const formattedDate = new Date(`${body.date}T${body.time}:00.000Z`); // ISO-8601 format
 
+        const existingBooking = await prisma.booking.findFirst({
+            where: {
+                name: body.name,
+                email: body.email,
+                phone: body.phone,
+                date: new Date(`${body.date}T${body.time}:00.000Z`)
+            }
+        });
+
+        if (existingBooking) {
+            return Response.json({ error: "Esiste già una prenotazione per questa data!" }, { status: 400 });
+        }
+
+        
         const booking = await prisma.booking.create({
             data: {
                 name: body.name,
